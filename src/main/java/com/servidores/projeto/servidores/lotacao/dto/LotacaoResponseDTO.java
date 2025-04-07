f8eee1f61ff646631744d249879407c5d5f3cab5
bruno <brunocrfraga@gmail.com>
2025-04-07 11:03:57 -0300

    Inclusão de construtores no response dto de lotação

--- src/main/java/com/servidores/projeto/servidores/lotacao/dto/LotacaoResponseDTO.java
+++ src/main/java/com/servidores/projeto/servidores/lotacao/dto/LotacaoResponseDTO.java
@@ -2,13 +2,17 @@ package com.servidores.projeto.servidores.lotacao.dto;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class LotacaoResponseDTO {
     private Long id;
